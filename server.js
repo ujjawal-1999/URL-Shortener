7bc6cc93021963697ff949a427767b45df2bea41
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-05-29 14:16:21 +0530

    MLab Setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 const ShortUrl = require('./models/ShortUrl');
 
-mongoose.connect('mongodb://localhost/urlShortener',{
+mongoose.connect('process.env.MONGODB_URI || mongodb://localhost/urlShortener',{
 	useNewUrlParser: true, useUnifiedTopology:true
 });
 

84bc9f333e2ddc15a9f7a4981ca1a5fa85d16859
Ujjawal Jain <jainujjawal1999@gmail.com>
2020-05-29 13:53:24 +0530

    URL Shortener

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+const ShortUrl = require('./models/ShortUrl');
+
+mongoose.connect('mongodb://localhost/urlShortener',{
+	useNewUrlParser: true, useUnifiedTopology:true
+});
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({ extended:false}));
+
+app.get('/',async (req,res) => {
+	const shortUrls = await ShortUrl.find();
+	res.render('index',{shortUrls});
+});
+
+app.post('/shortUrls',async (req,res) => {
+	await ShortUrl.create({full:req.body.fullUrl});
+	res.redirect('/');
+});
+
+app.get('/:shortUrl',async (req,res)=>{
+	const shortUrl = await ShortUrl.findOne({short : req.params.shortUrl});
+	if(shortUrl == null)
+		return res.sendStatus(404);
+	shortUrl.clicks++;
+	shortUrl.save().then(res=>console.log(res)).catch(err=>console.log(err));
+	res.redirect(shortUrl.full);
+})
+
+app.listen(port,()=>{
+	console.log('Server up and listening');
+})
